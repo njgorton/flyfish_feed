253877c23770e8f7425e90d6cbc79906fb6b2bc9
njgorton <gorton.nj@gmail.com>
2019-08-24 20:06:10 -0600

    responsive down to 600px vw

--- src/components/ScrollArrows.js
+++ src/components/ScrollArrows.js
@@ -3,12 +3,12 @@ import React from 'react'
 const ScrollArrows = ({ scrollUp, scrollDown }) => {
     return (
         <div className="scrollArrows__container">
-            <div className="scrollArrows scrollArrows--top" onMouseDown={scrollUp}>
+            <div className="scrollArrows scrollArrows--top" onClick={scrollUp}>
                 <div className="scrollArrows__leftSide scrollArrows__leftSide--top"></div>
                 <div className="scrollArrows__rightSide scrollArrows__rightSide--top"></div>
             </div>
 
-            <div className="scrollArrows scrollArrows--bottom" onMouseDown={scrollDown}>
+            <div className="scrollArrows scrollArrows--bottom" onClick={scrollDown}>
                 <div className="scrollArrows__leftSide scrollArrows__leftSide--bottom"></div>
                 <div className="scrollArrows__rightSide scrollArrows__rightSide--bottom"></div>
             </div>
